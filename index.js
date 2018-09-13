661c62f5184c2396aa9dd9ef708fbaf3f7e53243
meghan.cioci@wework.com <meghan.cioci@wework.com>
2018-09-13 23:27:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,28 @@ function createText2(event) {
   document.getElementById("lname1Display").innerText = textValue;
 }
 
+//FIRST NAME SECOND PERSON
+console.log("document.getElementById('fname1Entry')", document.getElementById("fname1Entry"))
+document.getElementById("fname1Entry").addEventListener("keyup", createText);
+var nameString = ""
+function createText(event) {
+  console.log("== createText ==")
+  // console.log("event.which:", event.which);
+  // console.log(String.fromCharCode(event.which));
+  let textValue = document.getElementById("fname1Entry").value;
+  document.getElementById("fname1Display").innerText = textValue;
+}
+
+//LAST NAME SECOND PERSON
+console.log("document.getElementById('lname1Entry')", document.getElementById("lname1Entry"))
+document.getElementById("lname1Entry").addEventListener("keyup", createText2);
+var nameString = ""
+function createText2(event) {
+  console.log("== createText2 ==")
+  let textValue = document.getElementById("lname1Entry").value;
+  document.getElementById("lname1Display").innerText = textValue;
+}
+
 // document.getElementById("fnameEntry").addEventListener("onclick", createText);
 // let divBox = document.getElementById("invitation");
 // let newDiv = document.createElement("invitation2");
