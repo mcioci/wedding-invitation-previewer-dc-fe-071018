898d843b8fb57a9979b0f740c9334ca91ee1e9ff
meghan.cioci@wework.com <meghan.cioci@wework.com>
2018-09-13 23:28:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,25 +21,23 @@ function createText2(event) {
 }
 
 //FIRST NAME SECOND PERSON
-console.log("document.getElementById('fname1Entry')", document.getElementById("fname1Entry"))
-document.getElementById("fname1Entry").addEventListener("keyup", createText);
+console.log("document.getElementById('fname2Entry')", document.getElementById("fname2Entry"))
+document.getElementById("fname2Entry").addEventListener("keyup", createText3);
 var nameString = ""
-function createText(event) {
-  console.log("== createText ==")
-  // console.log("event.which:", event.which);
-  // console.log(String.fromCharCode(event.which));
-  let textValue = document.getElementById("fname1Entry").value;
-  document.getElementById("fname1Display").innerText = textValue;
+function createText3(event) {
+  console.log("== createText3 ==")
+  let textValue = document.getElementById("fname2Entry").value;
+  document.getElementById("fname2Display").innerText = textValue;
 }
 
 //LAST NAME SECOND PERSON
-console.log("document.getElementById('lname1Entry')", document.getElementById("lname1Entry"))
-document.getElementById("lname1Entry").addEventListener("keyup", createText2);
+console.log("document.getElementById('lname2Entry')", document.getElementById("lname2Entry"))
+document.getElementById("lname2Entry").addEventListener("keyup", createText4);
 var nameString = ""
-function createText2(event) {
-  console.log("== createText2 ==")
-  let textValue = document.getElementById("lname1Entry").value;
-  document.getElementById("lname1Display").innerText = textValue;
+function createText4(event) {
+  console.log("== createText4 ==")
+  let textValue = document.getElementById("lname2Entry").value;
+  document.getElementById("lname2Display").innerText = textValue;
 }
 
 // document.getElementById("fnameEntry").addEventListener("onclick", createText);
