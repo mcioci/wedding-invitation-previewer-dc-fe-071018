158ea1bfe8cb5810eb476e7efb97ce61a951207c
meghan.cioci@wework.com <meghan.cioci@wework.com>
2018-09-13 23:15:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("document.getElementById('fnameEntry')", document.getElementById("fnameEntry"))
-document.getElementById("fnameEntry").addEventListener("keydown", createText);
+document.getElementById("fnameEntry").addEventListener("keyup", createText);
 var nameString = ""
 function createText(event) {
   console.log("== createText ==")
@@ -9,6 +9,8 @@ function createText(event) {
   document.getElementById("fnameDisplay").innerText = textValue;
 }
 
+
+document.getElementById("fnameEntry").addEventListener("onclick", createText);
 let divBox = document.getElementById("invitation");
 let newDiv = document.createElement("invitation2");
 divBox.appendChild(newDiv);
